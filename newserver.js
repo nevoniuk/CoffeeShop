654b0dc230555c3baf41b922034474e1b9d98d8a
nevoniuk <54695187+nevoniuk@users.noreply.github.com>
2021-11-02 16:20:19 -0400

    Update newserver.js

--- newserver.js
+++ newserver.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const app = express();
+
+mongoose.connect("mongodb+srv://testuser:B2usPpjfRDfvcEWx@coffeeshop.tkunn.mongodb.net/CoffeeShop");
+//.then((result) => console.log('connected to db'))
+//.catch((err) => console.log(err));
+app.listen(3000);
+
 
